b0a7593b889d4b3dc856ab3709b91c207f83c0d5
Christhopher Porpino <cgporpinof@gmail.com>
2020-11-04 18:10:27 -0300

    Updates

---
+++ precache-manifest.88a0ed4b7eed661e84e55b76b17a61c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6f7309d0a8889bf15b48f75902f0b2",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "2924b95c5e5e9b4f94b2",
+    "url": "/landingpage/static/css/main.285c2d55.chunk.css"
+  },
+  {
+    "revision": "36af536dc6c0bffc757a",
+    "url": "/landingpage/static/js/2.816d0b35.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/landingpage/static/js/2.816d0b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2924b95c5e5e9b4f94b2",
+    "url": "/landingpage/static/js/main.6a94bae8.chunk.js"
+  },
+  {
+    "revision": "a940d800f804626883ab",
+    "url": "/landingpage/static/js/runtime-main.4bd23e60.js"
+  },
+  {
+    "revision": "2375fe1d34da3efa9df332e19909b2f6",
+    "url": "/landingpage/static/media/Carol.2375fe1d.jpeg"
+  },
+  {
+    "revision": "e2b67daf33553781de0acd4d652ca18d",
+    "url": "/landingpage/static/media/Chris.e2b67daf.jpeg"
+  },
+  {
+    "revision": "9d8b120ae9245d5a240d56a1b3e1a85e",
+    "url": "/landingpage/static/media/Victor.9d8b120a.jpg"
+  },
+  {
+    "revision": "f6c6bf7fb57a987ba5fbf5d6c673dcc2",
+    "url": "/landingpage/static/media/illustration_programmer.f6c6bf7f.png"
+  }
+]);
