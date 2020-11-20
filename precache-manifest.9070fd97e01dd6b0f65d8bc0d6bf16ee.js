cce73ab40aa596fe8d6a3710cdcc71755d61078a
agenciaaudax <info@nanhekumar.com>
2020-11-20 13:55:28 -0300

    Updates

--- precache-manifest.9070fd97e01dd6b0f65d8bc0d6bf16ee.js
+++ precache-manifest.9070fd97e01dd6b0f65d8bc0d6bf16ee.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd6f7309d0a8889bf15b48f75902f0b2",
+    "revision": "7415ef94972e3f7b720138f17b491a27",
     "url": "/landingpage/index.html"
   },
   {
-    "revision": "2924b95c5e5e9b4f94b2",
+    "revision": "f471e42bb91911420493",
     "url": "/landingpage/static/css/main.285c2d55.chunk.css"
   },
   {
-    "revision": "36af536dc6c0bffc757a",
-    "url": "/landingpage/static/js/2.816d0b35.chunk.js"
+    "revision": "888e2cc54af52d52a37a",
+    "url": "/landingpage/static/js/2.f9cd1a4e.chunk.js"
   },
   {
     "revision": "8d190e2d02f8e768921a63f56a741895",
-    "url": "/landingpage/static/js/2.816d0b35.chunk.js.LICENSE.txt"
+    "url": "/landingpage/static/js/2.f9cd1a4e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2924b95c5e5e9b4f94b2",
-    "url": "/landingpage/static/js/main.6a94bae8.chunk.js"
+    "revision": "f471e42bb91911420493",
+    "url": "/landingpage/static/js/main.4edee572.chunk.js"
   },
   {
-    "revision": "a940d800f804626883ab",
-    "url": "/landingpage/static/js/runtime-main.4bd23e60.js"
+    "revision": "f5131ef8905b098f4435",
+    "url": "/landingpage/static/js/runtime-main.494a572f.js"
   },
   {
     "revision": "2375fe1d34da3efa9df332e19909b2f6",
@@ -34,6 +34,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "9d8b120ae9245d5a240d56a1b3e1a85e",
     "url": "/landingpage/static/media/Victor.9d8b120a.jpg"
+  },
+  {
+    "revision": "049df64e7ddef01c541948689e07f025",
+    "url": "/landingpage/static/media/audax-logo-white.049df64e.png"
   },
   {
     "revision": "f6c6bf7fb57a987ba5fbf5d6c673dcc2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/landingpage/precache-manifest.88a0ed4b7eed661e84e55b76b17a61c5.js"
+  "/landingpage/precache-manifest.9070fd97e01dd6b0f65d8bc0d6bf16ee.js"
 );
 
 self.addEventListener('message', (event) => {
