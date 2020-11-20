c2511940629743263428fbe9f7814f86385cfa67
agenciaaudax <info@nanhekumar.com>
2020-11-20 13:44:35 -0300

    Updates

---
+++ precache-manifest.b4f3d8a43bca6ecc01b6ba2725663499.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a6d84246abc5724c854655b737558a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35daa125fde2399afca",
+    "url": "/static/css/main.285c2d55.chunk.css"
+  },
+  {
+    "revision": "c261b1cfb7bae7814a53",
+    "url": "/static/js/2.badb9d5c.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.badb9d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35daa125fde2399afca",
+    "url": "/static/js/main.0cadc507.chunk.js"
+  },
+  {
+    "revision": "1d481250d64de828ac95",
+    "url": "/static/js/runtime-main.6d053a21.js"
+  },
+  {
+    "revision": "2375fe1d34da3efa9df332e19909b2f6",
+    "url": "/static/media/Carol.2375fe1d.jpeg"
+  },
+  {
+    "revision": "e2b67daf33553781de0acd4d652ca18d",
+    "url": "/static/media/Chris.e2b67daf.jpeg"
+  },
+  {
+    "revision": "9d8b120ae9245d5a240d56a1b3e1a85e",
+    "url": "/static/media/Victor.9d8b120a.jpg"
+  },
+  {
+    "revision": "049df64e7ddef01c541948689e07f025",
+    "url": "/static/media/audax-logo-white.049df64e.png"
+  },
+  {
+    "revision": "f6c6bf7fb57a987ba5fbf5d6c673dcc2",
+    "url": "/static/media/illustration_programmer.f6c6bf7f.png"
+  }
+]);
