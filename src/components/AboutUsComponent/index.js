3bf1bf11107b78cc0a4c8e60f998b15fc1c04e77
agenciaaudax <agenciaaudax@gmail.com>
2020-11-19 21:58:11 -0300

    update

--- src/components/AboutUsComponent/index.js
+++ src/components/AboutUsComponent/index.js
@@ -11,8 +11,17 @@ const AboutUs = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 765px;
+  height: 800px;
   width: 100%;
+
+  @media (max-width: 700px)
+  {
+
+width: 375px!important;
+height: 1509px!important;
+margin-bottom: 0px;
+
+}
 `;
 
 const Title = styled.h1`
@@ -20,9 +29,17 @@ const Title = styled.h1`
   font-weight: 600;
   font-size: 38px;
   line-height: 46px;
-  margin-bottom: 32px;
   text-align: center;
   padding-top: 72px;
+  
+  @media (max-width: 700px)
+  {
+    text-align: center;
+    font-size: 30px;
+    padding: 0;
+    margin-top: 56px;
+
+  }
 `;
 
 const SecondaryButton = styled.button`
@@ -31,15 +48,49 @@ const SecondaryButton = styled.button`
   background: #ffffff;
   border: 1px solid #03045e;
   border-radius: 30px;
+
   font-family: "Poppins", sans-serif;
   font-weight: bold;
   font-size: 18px;
   line-height: 27px;
   color: #03045e;
+
+  transition-duration: 0.3s;
+  box-shadow: 0 8px 16px 0 rgba(3, 4, 94, 0.2),
+    0 6px 20px 0 rgba(3, 4, 94, 0.19);
+
+  &:hover {
+    background: #555687;
+    color: #ffffff;
+    box-shadow: 0 6px 50px #555687;
+  }
+
+  &:active {
+    background: #f0f0ff;
+  }
+
+  @media (max-width: 700px)
+  {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+width: 160px;
+height: 39px;
+margin: 32px 0px 56px 0px;
+
+}
 `;
 
 const CardsBox = styled.div`
   display: flex;
+
+
+  @media (max-width: 700px)
+  {
+  display: flex!important;
+flex-direction: column!important;
+
+}
 `;
 
 function AboutUsComponent() {
@@ -66,8 +117,7 @@ function AboutUsComponent() {
           photo={victor}
           name="Victor Tarroni"
           skills="Front End Developer aplicando seus conhecimentos de CSS, HTML e
-          JavaScript com muita técnica e visão comercial. Tem pratica em SEO e
-          conhecimento comercial."
+          JavaScript com muita técnica e visão comercial."
         />
       </CardsBox>
       <SecondaryButton>QUERO UM SITE</SecondaryButton>
